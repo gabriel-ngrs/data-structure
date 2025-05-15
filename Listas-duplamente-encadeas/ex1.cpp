92412e9744cb856c2afb9e457be418685ee21774
Gabriel Negreiros <gabrielnegreirossaraiva38@gmail.com>
2025-05-15 18:56:36 -0300

    metodos inserir_inicio/final de uma lista duplamente encadeada

--- Listas-duplamente-encadeas/ex1.cpp
+++ Listas-duplamente-encadeas/ex1.cpp
@@ -14,6 +14,7 @@ class ListaDuplamenteEncadeada {
     public:
         ListaDuplamenteEncadeada() {
             inicio = nullptr; // Como a lista esta vazia, o inicio aponta direto para o nullptr
+            fim = nullptr;
             cout << "Lista criada com sucesso!" << endl;
         }
 
@@ -27,5 +28,43 @@ class ListaDuplamenteEncadeada {
             cout << "Memoria liberada com sucesso!" << endl;
         }
 
+        void inserir_inicio(int valor) {
+            No* novo = new No;  // Alocando memoria
+            novo->dado = valor;
 
+            if (inicio == nullptr) {  // Verificando se a lista esta vazia 
+                inicio = novo;  // Inicio agora aponta para o novo no
+                novo->proximo = nullptr;  // Ponteiro proximo do novo no aponta agora para nullptr pois ele é o primeiro e unico elemento
+                novo->anterior = nullptr;  // Ponteiro anterior do novo no aponta agora para nullptr pois ele é o primeiro e unico elemento
+                fim = novo;  // Fim agora aponta para o novo no, pois a lista estava vazia, logo esse no é o inicio e fim da lista
+                cout << "Elemento inserido com sucesso." << endl;
+                return;
+            }
+
+            novo->proximo = inicio;  // Ponteiro proximo do novo no aponta agora para o proximo elemente, que era antes o inicio
+            inicio->anterior = novo;  // Ponteiro anterior do 2 no, que no caso era o inicio agora aponta para o novo
+            novo->anterior = nullptr;  // Anterior do novo aponta pra null pois agora é o primeiro no
+            inicio = novo;  // Inicio agora aponta para o novo no
+            cout << "Elemento inserido com sucesso." << endl;
+        }   
+
+        void inserir_final(int valor) {
+            No* novo = new No;
+            novo->dado = valor;
+
+            if(inicio == nullptr) {  // Verificando se a lista esta vazia
+                inicio = novo;  // Usando mesma logica do metodo inserir_inicio
+                novo->proximo = nullptr;
+                novo->anterior = nullptr;
+                fim = novo;
+                cout << "Elemento inserido com sucesso." << endl;
+                return;
+            }
+
+            novo->proximo = nullptr;  // O novo no agora aponta para o nullptr pois é agora o ultimo no
+            novo->anterior = fim;  // O seu ponteiro anterior aponta para o antigo ultimo no
+            fim->proximo = novo;  // O ponteiro proximo do antigo ultimo no agora aponta para o novo no
+            fim = novo;  // O fim agora e o novo no
+            cout << "Elemento inserido com sucesso." << endl;
+        }
 };
